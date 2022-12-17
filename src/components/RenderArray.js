85f46f66b2d41f0a6588caf63decac3fdc6a6d08
Rae Hebron <rae.oxford.hebron@gmail.com>
2022-12-17 15:29:07 -0500

    add global css

--- src/components/RenderArray.js
+++ src/components/RenderArray.js
@@ -2,10 +2,12 @@ import Card from "./Card"
 
 function RenderArray(props){
     return (<>
+    <div className="card-container">
     {props.data.map((item, index) => 
         <Card item={item} index={index}/>
         
     )}
+    </div>
     </>)
 }
 
